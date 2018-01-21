6f0a9b4869fb15be425d3cf010a4d3bc8e9e4010
BlackCap <mailhph@gmail.com>
2018-01-21 02:18:21 +0100

    Open the game in a browser on another machine via ssh

--- src/Vindinium/Play.hs
+++ src/Vindinium/Play.hs
@@ -9,16 +9,25 @@ import Vindinium.Types
 import Vindinium.Api
 import Control.Monad.IO.Class (liftIO)
 import Data.List.Split (chunksOf)
+import qualified Data.Text as T
+import Control.Monad
+import System.Process (spawnCommand)
 
 playTraining :: Maybe Int -> Maybe Board -> Bot -> Vindinium State
-playTraining mt mb b = startTraining mt mb >>= playLoop b
+playTraining mt mb b = do
+  st <- startTraining mt mb
+  beforeGame st
+  playLoop b st
 
 playArena :: Bot -> Vindinium State
-playArena b = startArena >>= playLoop b
+playArena b = do
+  st <- startArena
+  beforeGame st
+  playLoop b st
 
 playLoop :: Bot -> State -> Vindinium State
 playLoop bot state = do
-  liftIO $ putStrLn . drawBoard . gameBoard $ stateGame state
+  liftIO . putStrLn . drawBoard . gameBoard $ stateGame state
 
   if (gameFinished . stateGame) state
       then return state
@@ -26,6 +35,14 @@ playLoop bot state = do
           newState <- bot state >>= move state
           playLoop bot newState
 
+beforeGame :: State -> Vindinium ()
+beforeGame st = do
+  liftIO . showGame $ stateViewUrl st
+  return ()
+
+----------------
+
+-- Render the game as ASCII art
 drawBoard :: Board -> String
 drawBoard (Board s ts)
   = unlines $ chunksOf s $ map f ts
@@ -35,3 +52,6 @@ drawBoard (Board s ts)
           TavernTile  -> 't'
           MineTile _  -> 'm'
           HeroTile (HeroId i) -> head $ show i
+
+-- Open the game in a browser on a remote monitor
+showGame url = void $ spawnCommand ("ssh blackcap@158.36.81.46 'DISPLAY=:0 chromium " ++ T.unpack url ++ "'")
